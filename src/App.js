ced08f43d685502d96a7fc9ae0f570f621d7533a
Giovanni Mauro <giovannimauro95@hotmail.it>
2024-11-25 14:42:35 +0100

    Compito Finito

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import ImgComponent from "./components/ImgComponent.jsx"
+import ButtonComponent from "./components/ButtonComponent.jsx"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonComponent label="Pippo Baudo" />
+      <ImgComponent src="https:/placecats.com/200/200" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,10 @@
+const ButtonComponent = (props) => {
+    return (
+        <section>
+        <h3>Bottone</h3>
+        <button> {props.label}</button>
+        </section>
+    )
+}
+
+export default ButtonComponent
